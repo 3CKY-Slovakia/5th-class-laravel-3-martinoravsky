48e5847398d58fd6bf2866e86caf6dbe4fd9409d
3CKY-Kmetko <jakub.kmetko@3cky.sk>
2015-10-07 17:30:22 +0200

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta charset="utf-8' />
+    <title>STI LABS @section('title')</title>
+    <meta name="description" content=" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!--Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}" />
+
+    <!-- /css Files -->
+
+    <link rel="stylesheet" href="{{ asset('/css/reset.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/socials.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/flexslider.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/simpletextrotator.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/cubeportfolio.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/timeline.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/owl.carousel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/settings-ie8.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/settings.css') }}" />
+    <!-- Page Styles -->
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/backgrounds.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/css/responsive.css') }}" />
+    <!-- End Page Styles -->
+
+    <!-- Page Layout Color, night or dark -->
+    <link id="changeable_tone" rel="stylesheet" href="{{ asset('/css/page_tones/dark.css') }}" />
+    <!-- End Page Layout Color -->
+
+    <!-- Page Elements Color -->
+    <link id="changeable_color" rel="stylesheet" href="{{ asset('/css/colors/red.css') }}" />
+    <!-- End Page Elements Color -->
+
+    <!-- Page Fonts / Pacifico-->
+    <link href='http://fonts.googleapis.com//css?family=Pacifico' rel='stylesheet' type='text//css'>
+    <!-- Raleway-->
+    <link href='http://fonts.googleapis.com//css?family=Raleway:400,100,,200,300,600,700' rel='stylesheet' type='text//css'>
+
+    <!-- End /css Files -->
+
+</head>
+
+<!-- Body Start -->
+<body class="parallax">
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- JS Files -->
+    <script type="text/javascript" src="{{ asset('/js/jquery-2.1.3.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.appear.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/waypoint.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/modernizr-latest.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.easing.1.3.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/SmoothScroll.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.magnific-popup.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.superslides.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.flexslider.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.simple-text-rotator.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.cubeportfolio.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.parallax-1.1.3.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/skrollr.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.fitvids.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.mb.YTPlayer.js') }}"></script>
+    <!-- Contact Form -->
+    <script type="text/javascript" src="{{ asset('/js/jquery.validate.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/contact-form.js') }}"></script>
+    <!-- Twitter -->
+    <script type="text/javascript" src="{{ asset('/js/tweecool.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/tweecool.js') }}"></script>
+    <!-- Page Plugins -->
+    <script type="text/javascript" src="{{ asset('/js/plugins.js') }}"></script>
+    <!-- Portfolio Plugins -->
+    <script type="text/javascript" src="{{ asset('/js/portfolio.js') }}"></script>
+    <!-- Theme Panel -->
+    <script type="text/javascript" src="{{ asset('/theme_panel/themepanel-sub.js') }}"></script>
+    <!-- End JS Files -->
+
+    <script>
+        $(document).ready(function() {
+            $('#latest_tweets').tweecool({
+                username : 'envato'
+            });
+        });
+    </script>
+
+
+</body>
+<!-- Body End -->
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="content">
+        <div class="title">Laravel 5</div>
+    </div>
+@stop
